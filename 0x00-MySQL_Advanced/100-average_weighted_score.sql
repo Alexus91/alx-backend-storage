a8117c349ab41d6f890713638df2c48610bab8f6
root <Alexus91>
2024-01-19 01:27:04 +0000

    12. Average weighted score

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,30 @@
+-- Update the user's average_score in the users table
+-- Calculate average weighted score (avoid division by zero)
+
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id_param INT)
+BEGIN
+    DECLARE total_weighted_score FLOAT;
+    DECLARE total_weight INT;
+    DECLARE avg_weighted_score FLOAT;
+
+    SELECT SUM(corrections.score * projects.weight), SUM(projects.weight)
+    INTO total_weighted_score, total_weight
+    FROM corrections
+    JOIN projects ON corrections.project_id = projects.id
+    WHERE corrections.user_id = user_id_param;
+
+    IF total_weight > 0 THEN
+        SET avg_weighted_score = total_weighted_score / total_weight;
+    ELSE
+        SET avg_weighted_score = 0;
+    END IF;
+
+    UPDATE users
+    SET average_score = avg_weighted_score
+    WHERE id = user_id_param;
+
+END //
+
+DELIMITER ;
