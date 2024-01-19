bd73bc0707e56798725f74de2b85a14c9fb42b38
root <Alexus91>
2024-01-19 01:35:38 +0000

    13. Average weighted score for all!

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,8 +1,6 @@
 -- Update the user's average_score in the users table
 -- Calculate average weighted score (avoid division by zero)
 
-DELIMITER //
-
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id_param INT)
 BEGIN
     DECLARE total_weighted_score FLOAT;
@@ -26,5 +24,3 @@ BEGIN
     WHERE id = user_id_param;
 
 END //
-
-DELIMITER ;
---
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -0,0 +1,25 @@
+-- SQL script that creates a stored procedure ComputeAverageWeightedScoreForUsers
+-- computes and store the average weighted score for all students.
+
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
+BEGIN
+    DECLARE user_id_param INT;
+    DECLARE done INT DEFAULT FALSE;
+    DECLARE cur CURSOR FOR SELECT id FROM users;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+    OPEN cur;
+
+    user_loop: LOOP
+        FETCH cur INTO user_id_param;
+        
+        IF done THEN
+            LEAVE user_loop;
+        END IF;
+
+        CALL ComputeAverageWeightedScoreForUser(user_id_param);
+    END LOOP;
+
+    CLOSE cur;
+
+END //
