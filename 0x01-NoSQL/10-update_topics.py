5e295240202838eea0130834ce116559dc8dca97
root <Alexus91>
2024-01-24 01:44:28 +0000

    10. Change school topics

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Change all topics of a school document based on the name."""
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    update many rows pymongo collection object
+    """
+    return mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
