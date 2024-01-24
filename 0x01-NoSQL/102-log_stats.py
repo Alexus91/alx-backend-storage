ec0ffa79f5cca3a0e8705bb20fe7c93fa893cce5
root <Alexus91>
2024-01-24 02:26:32 +0000

    15. Log stats - new version

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+Script that provides stats about Nginx logs stored in MongoDB
+"""
+
+from pymongo import MongoClient
+
+def log_stats():
+    """
+    Display statistics about Nginx logs in MongoDB
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
+    collection = db.nginx
+
+    
+    total_logs = collection.count_documents({})
+    print("{} logs".format(total_logs))
+
+    
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, count))
+
+
+    status_check_count = collection.count_documents({"method": "GET", "path": "/status"})
+    print("{} status check".format(status_check_count))
+
+
+    top_ips = collection.aggregate([
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ])
+
+    print("IPs:")
+    for ip_info in top_ips:
+        print("\t{}: {}".format(ip_info["_id"], ip_info["count"]))
+
+if __name__ == "__main__":
+    log_stats()
